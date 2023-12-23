2955b7b4567134cce541b72c9df70d6408f6e2f6
Srinath10X <srinathcreates@gmail.com>
2023-12-23 19:52:22 +0530

    Added nvim tree and telescope

---
+++ lua/plugins/nvimtree.lua
@@ -0,0 +1,10 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		-- disable netrw
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		-- setup function
+		require("nvim-tree").setup()
+	end,
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+					},
+				},
+			},
+		})
+	end,
+}
