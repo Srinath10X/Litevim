8f2a57e5cab7c6e134c53599965b4c65437e19c8
Srinath10X <srinathcreates@gmail.com>
2023-12-24 13:00:31 +0530

    Added snippets and with cmp buffer cmp path plugins etc

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 require("core.options")
 require("core.keymaps")
 require("lite")
+require("plugins.lsp.cmp")
+require("plugins.cmp")
---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,12 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"L3MON4D3/LuaSnip", --snippet engine
+		"saadparwaiz1/cmp_luasnip", --nvim_cmp source
+		"hrsh7th/cmp-nvim-lsp", -- lsp completion capabilites
+		"hrsh7th/cmp-buffer", -- buffer completions
+		"hrsh7th/cmp-path", -- path completions
+		"rafamadriz/friendly-snippets", -- friendly snippets
+		"onsails/lspkind.nvim", -- vs code like icons
+	},
+}
