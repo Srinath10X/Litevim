8ef559b09277cdf541293a3446e0c3979e93dd42
Srinath10X <srinathcreates@gmail.com>
2023-12-25 13:21:03 +0530

    Fixed neovide zooming issue and added docs

---
+++ lua/core/neovide.lua
@@ -0,0 +1,16 @@
+-- neovide options to zoom in and zoom out fixed
+if vim.g.neovide == true then
+	vim.api.nvim_set_keymap(
+		"n",
+		"<C-+>",
+		":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+		{ silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"<C-->",
+		":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+		{ silent = true }
+	)
+	vim.api.nvim_set_keymap("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+end
---
+++ lua/plugins/user.lua
@@ -0,0 +1,2 @@
+-- User plugins here
+return {}
