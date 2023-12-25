a57cde053501d97597bcdc42b47cfe026913c71a
Srinath10X <srinathcreates@gmail.com>
2023-12-25 14:46:13 +0530

    Added Lazygit

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -19,7 +19,7 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  >  New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("f", "󰍉  >  Find file", ":cd $HOME/Workspace | Telescope find_files<CR>"),
+			dashboard.button("f", "󰍉  >  Find file", ":cd $HOME/code | Telescope find_files<CR>"),
 			dashboard.button("r", "  >  Recent", ":Telescope oldfiles<CR>"),
 			dashboard.button("s", "  >  Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
 			dashboard.button("q", "󰗼  >  Quit NVIM", ":qa<CR>"),
