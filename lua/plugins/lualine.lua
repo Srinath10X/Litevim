867d439e096b3c7af2ad3296bab95633ab84c435
Srinath10X <srinathcreates@gmail.com>
2023-12-26 22:28:53 +0530

    Customized lualine to bubble theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,5 @@
 -- lualine
+
 return {
 	-- Set lualine as statusline
 	"nvim-lualine/lualine.nvim",
@@ -12,18 +13,44 @@ return {
 			theme = "catppuccin",
 			disabled_filetypes = { "NvimTree" },
 			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
 		},
+
+		--[[
+	
+		            Lualine has sections as shown below. 
+		  ╭─────────────────────────────────────────────────────────╮
+		  │   +-------------------------------------------------+		│
+		  │   | A | B | C                             X | Y | Z |		│
+		  │   +-------------------------------------------------+		│
+		  ╰─────────────────────────────────────────────────────────╯
+
+		--]]
+
 		sections = {
 			lualine_a = {
-				{ icon = "", "mode", right_padding = 2 },
+				{
+					icon = "",
+					"mode",
+					right_padding = 2,
+					separator = {
+						left = "",
+						right = "",
+					},
+				},
 			},
-
 			lualine_b = {
-				{ "filetype", icon_only = true, color = { fg = "#bcc0cc" } },
-				{ "filename", color = { fg = "#bcc0cc" } },
+				{
+					"filetype",
+					icon_only = true,
+					color = { fg = "#bcc0cc" },
+				},
+				{
+					"filename",
+					color = { fg = "#bcc0cc" },
+				},
 			},
-
-			lualine_c = { "fileformat" },
+			lualine_c = {},
 		},
 	},
 }
