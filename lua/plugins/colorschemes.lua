ac81a93d6e2329db2a6947318cbdd28d31235b6c
Srinath10X <srinathcreates@gmail.com>
2024-01-26 12:04:15 +0530

    Updated config

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,25 +1,35 @@
 return {
-	-- Colorscheme's
-	{
-		"catppuccin/nvim", -- catppuccin
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+  -- Colorscheme's
+  {
+    "catppuccin/nvim", -- catppuccin
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 
-	{
-		"LunarVim/lunar.nvim",
-	},
+  {
+    "LunarVim/lunar.nvim", -- lunar theme
+  },
 
-	{
-		"shaunsingh/nord.nvim", -- nord
-		name = "nord",
-	},
+  {
+    "shaunsingh/nord.nvim", -- nord theme
+    name = "nord",
+  },
 
-	{
-		"Mofiqul/vscode.nvim", -- vscode
-		name = "vscode",
-	},
+  {
+    "Mofiqul/vscode.nvim", -- vscode
+    name = "vscode",
+  },
+
+  {
+    "folke/tokyonight.nvim", -- tokyonight
+    name = "tokyonight",
+  },
+
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+  },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,22 @@
 -- lualine
 
 return {
-	-- Set lualine as statusline
-	"nvim-lualine/lualine.nvim",
-	-- See `:help lualine.txt`
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-		options = {
-			icons_enabled = true,
-			theme = "catppuccin",
-			disabled_filetypes = { "NvimTree" },
-			component_separators = { left = "", right = "" },
-			section_separators = { left = "", right = "" },
-		},
+  -- Set lualine as statusline
+  "nvim-lualine/lualine.nvim",
+  -- See `:help lualine.txt`
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = "catppuccin",
+      disabled_filetypes = { "NvimTree" },
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    },
 
-		--[[
+    --[[
 	
 		            Lualine has sections as shown below. 
 		  ╭─────────────────────────────────────────────────────────╮
@@ -27,39 +27,39 @@ return {
 
 		--]]
 
-		sections = {
-			lualine_a = {
-				{
-					icon = "",
-					"mode",
-					right_padding = 2,
-					separator = {
-						left = "",
-						right = "",
-					},
-				},
-			},
-			lualine_b = {
-				{
-					"filetype",
-					icon_only = true,
-					color = { fg = "#bcc0cc" },
-				},
-				{
-					"filename",
-					color = { fg = "#bcc0cc" },
-				},
-			},
-			lualine_c = {},
-			lualine_z = {
-				{
-					"location",
-					separator = {
-						left = "",
-						right = "",
-					},
-				},
-			},
-		},
-	},
+    sections = {
+      lualine_a = {
+        {
+          icon = "",
+          "mode",
+          right_padding = 2,
+          separator = {
+            left = "",
+            right = "",
+          },
+        },
+      },
+      lualine_b = {
+        {
+          "filetype",
+          icon_only = true,
+          color = { fg = "#bcc0cc" },
+        },
+        {
+          "filename",
+          color = { fg = "#bcc0cc" },
+        },
+      },
+      lualine_c = {},
+      lualine_z = {
+        {
+          "location",
+          separator = {
+            left = "",
+            right = "",
+          },
+        },
+      },
+    },
+  },
 }
