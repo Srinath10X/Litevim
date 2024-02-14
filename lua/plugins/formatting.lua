8cb7fcae295a44604f0b995257663e29f9a012c0
Srinath10X <srinathcreates@gmail.com>
2024-02-14 19:31:22 +0530

    Added formattiing for c language

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -20,6 +20,7 @@ return {
         graphql = { "prettier" },
         lua = { "stylua" },
         python = { "isort", "black" },
+        c = { "clangd" },
       },
       format_on_save = {
         lsp_fallback = true,
