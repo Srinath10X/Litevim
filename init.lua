76d5b396e71cdc3d52066f19bea5ceb453abb7d7
Srinath10X <srinathcreates@gmail.com>
2024-07-05 02:17:18 +0530

    configured new icons and updated lualine section

--- init.lua
+++ init.lua
@@ -1,27 +1,27 @@
 local Modules = {
-	core = {
-		"options",
-		"keymaps",
-	},
-	lazy = "lite",
-	plugins = {
-		lsp = {
-			"cmp",
-			"lsp",
-			"null-ls",
-		},
-	},
+  core = {
+    "options",
+    "keymaps",
+  },
+  lazy = "lite",
+  plugins = {
+    lsp = {
+      "cmp",
+      "lsp",
+      "null-ls",
+    },
+  },
 }
 
 local function requireModules(modules, prefix)
-	prefix = prefix or ""
-	for key, module in pairs(modules) do
-		if type(module) == "string" then
-			require(prefix .. module)
-		elseif type(module) == "table" then
-			requireModules(module, prefix .. key .. ".")
-		end
-	end
+  prefix = prefix or ""
+  for key, module in pairs(modules) do
+    if type(module) == "string" then
+      require(prefix .. module)
+    elseif type(module) == "table" then
+      requireModules(module, prefix .. key .. ".")
+    end
+  end
 end
 
 requireModules(Modules)
--- lua/lite.lua
+++ lua/lite.lua
@@ -18,7 +18,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Settign up leader key before loading lazy
 vim.g.mapleader = " "
 
 -- Setup lazy.nvim
