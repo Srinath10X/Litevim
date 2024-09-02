4e003ab9549eaa8623657517d6d81eb703c1eb28
Srinath10X <srinathcreates@gmail.com>
2024-09-02 13:37:15 +0530

    Fix: Neotree opening directory error

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -34,6 +34,18 @@ local M = {
       "MunifTanjim/nui.nvim",
     },
     cmd = "Neotree",
+    init = function()
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("NeoTreeInit", { clear = true }),
+        callback = function()
+          local f = vim.fn.expand("%:p")
+          if vim.fn.isdirectory(f) ~= 0 then
+            vim.cmd("Neotree current dir=" .. f)
+            vim.api.nvim_clear_autocmds({ group = "NeoTreeInit" })
+          end
+        end,
+      })
+    end,
     keys = {
       { "<leader>e", ":Neotree toggle reveal<CR>", silent = true, { desc = "NeoTree reveal" } },
     },
