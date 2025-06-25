4d7d1638b279d8f18e5bdc92a5fe7daf07982386
Srinath10X <srinathcreates@gmail.com>
2025-06-25 21:39:30 +0530

    refactor: added VimEnter event for vim-tmux-navigator

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -1,63 +1,65 @@
 local M = {
-  vim_tmux_navigator = {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
+	-- vim-tmux navigator
+	vim_tmux_navigator = {
+		"christoomey/vim-tmux-navigator",
+		event = "VimEnter",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 
-  -- Comment
-  comment = {
-    "numToStr/Comment.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {},
-  },
+	-- Comment
+	comment = {
+		"numToStr/Comment.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {},
+	},
 
-  -- ToggleTerm
-  toggle_term = {
-    "akinsho/toggleterm.nvim",
-    cmd = { "ToggleTerm" },
-    keys = {
-      { "<c-t>", "<cmd>ToggleTerm direction=float dir=.<cr>" },
-    },
-    opts = {
-      direction = "float",
-      shade_terminals = false,
-      close_on_exit = true,
-      float_opts = {
-        border = "curved",
-        winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
-      },
-    },
-  },
+	-- ToggleTerm
+	toggle_term = {
+		"akinsho/toggleterm.nvim",
+		cmd = { "ToggleTerm" },
+		keys = {
+			{ "<c-t>", "<cmd>ToggleTerm direction=float dir=.<cr>" },
+		},
+		opts = {
+			direction = "float",
+			shade_terminals = false,
+			close_on_exit = true,
+			float_opts = {
+				border = "curved",
+				winblend = 0,
+				highlights = {
+					border = "Normal",
+					background = "Normal",
+				},
+			},
+		},
+	},
 
-  -- Autopairs
-  autopairs = {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-  },
+	-- Autopairs
+	autopairs = {
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
 }
 
 return {
-  M.vim_tmux_navigator,
-  M.comment,
-  M.toggle_term,
-  M.autopairs,
-  M.markdown,
+	M.vim_tmux_navigator,
+	M.comment,
+	M.toggle_term,
+	M.autopairs,
+	M.markdown,
 }
